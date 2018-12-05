548bb412db80c29a19e4fdaef94eace82db156e2
daiannawoaini <leiyanjun3@163.com>
2018-12-05 22:22:53 +0800

    juzhongchuli

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -1,400 +1,302 @@
-<!-- 首页 -->
 <template>
-	<div class="Activity">
-	<div class="content">
-		<div class="title"><h1>行业热门活动</h1></div>
-		<div class="text"><h2>汇聚各大产业最新、最热、最前沿的盛会</h2></div>
-		<br>
-		<div class="globnav">
-			<ul>
-				<a id="first-item" href=""><li>活动类型</li></a>
-				<a href=""><li>不限</li></a>
-				<a href=""><li>展会论坛</li></a>
-				<a href=""><li>互动沙龙</li></a>
-				<a href=""><li>项目路演</li></a>
-				<a href=""><li>培训讲座</li></a>
-				<a href=""><li>走访考察</li></a>
-				<a href=""><li>线上分析</li></a>
-				<a href=""><li>其他</li></a>
-			</ul>
-		</div><br>
-		<div class="list">
-			<div class="left">
-				<img class="picture" src="https://res4.maikeji.cn/FssbvCnaFfUFW8ScbkpJ4cdxqXHl" alt="">
-			</div>
-			<div class="right">
-				<ul>
-					<li id="second">与资本零距离|创合会"商学院好项目"第十期路演会</li>
-					<li>活动时间：2018.12.09-2018.12.09</li>
-					<li>简介：迈科技，创合会强强联合，携手百家投资机构，助力创业者精准对接，实现创业梦想</li>
-					<li><button>我要报名</button></li>
-				</ul>
-			</div>
-
-		</div><br>
-		<div class="item">
-			<div class="item-1">
-				<img class="picture" src="https://res4.maikeji.cn/FvB-7IMYN14zo1DbhfrOwVwx5IgD" alt="">
-			</div>
-			<div class="item-2">
-				<ul>
-					<li id="third">【讲座】|吴晓波带你解读《避免失败》</li>
-					<li>活动时间：2018.12.11-2018.12.13</li>
-					<li>简介：现在报名享受会员优惠，赠送超值服务</li>
-					<li><button>我要报名</button></li>
-				</ul>
-			</div>
-		</div><br>
-		<div class="meet">
-			<div class="meet-1">
-				<img class="picture" src="https://res4.maikeji.cn/FhQtJGLFWeMtBaA9uiCZYasWDsYD" alt="">
-			</div>
-			<div class="meet-2">
-				<ul>
-					<li id="forth">第二届平湖市科技企业投融资对接会</li>
-					<li>活动时间：2018.12.11-2018.12.13</li>
-					<li>简介：科技赋能，激发创新活力，诚邀各位参加第二届平湖市科技企业投融资对接会</li>
-					<li><button>我要报名</button></li>
-				</ul>
-			</div>
-		</div><br>
-		<div class="filter">
-			<div class="filter-1">
-				<img class="picture" src="https://res4.maikeji.cn/Fhk0j9OoOT8IaQsP2pzjodCyS_0Q" alt="">
-			</div>
-			<div class="filter-2">
-				<ul>
-					<li id="fifth">让技术与市场来一次真正的对话|迈科技公益大讲堂</li>
-					<li>活动时间：2018.07.01-2018.12.31</li>
-					<li>简介：不要让优质项目埋没，真正让技术发挥价值</li>
-					<li><button>我要报名</button></li>
-				</ul>
-			</div>
-		</div><br>
-		<div class="read">
-			<div class="read-1">
-				<img class="picture" src="https://res4.maikeji.cn/Fk1kiY55VQDnvqHwxjfnOj8hm2Wm" alt="">
-			</div>
-			<div class="read-2">
-				<ul>
-					<li id="sixth">2019第十一届上海化工技术装备展览会</li>
-					<li>活动时间：2019.08.21-2019.08.23</li>
-					<li>简介：2019第十一届上海化工技术装备展览会开始报名啦</li>
-					<li><button>我要报名</button></li>
-				</ul>
-			</div>
-		</div><br>
-		<div class="page">
-			<ul>
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-				<li>4</li>
-				<li>5</li>
-				<li>6</li>
-				<li>27</li>
-				<li>...</li>
-				<li>下一页</li>
-			</ul>
-		</div>
-		
-	</div>
-	<div class="rightnav">
-			<div class="top">
-				<span>活动回顾</span>
-			</div>
-			<div class="top-1">
-				<img class="picture" src="https://res4.maikeji.cn/FpJ8zik8TTGnqipwgNp8m9UdKEtG" alt="">
-			</div>
-			<div class="top-2">
-				<span>需求征集中！第三方中国创新挑。。。</span>
-			</div>
-			<div class="top-3">
-				<img class="picture" src="https://res4.maikeji.cn/FkMmpPsTZit5R3ThgUm8LTOL2qpU" alt="">
-			</div>
-			<div class="top-4">
-				<span>【公开课】首届长三角技术交</span>
-			</div>
-			<div class="top-5">
-				<img class="picture" src="https://res4.maikeji.cn/FoPw9hs-MHTkKhurchvgU0XmaXec" alt="">
-			</div>
-			<div class="top-6">
-				<span>有机工业废水如何处理？看看</span>
-			</div>
-			<div class="top-7">
-				<img class="picture" src="https://www.maikeji.cn/images/wx-qrcode.jpg" alt="">
-			</div>
-		</div>
-
-	</div>
+  <div class="Activity">
+    <div class="container">
+    	<div class="title">
+    		<h1>行业热门活动</h1>
+    	</div>
+    	<div class="text">
+    		<h2>汇聚各大产业最新、 最热、 最前沿 的盛会</h2>
+    	</div>
+    	<div class="list">
+    		<ul>
+    			<a id="item-first" href=""><li>活动类型</li></a>
+    			<a id="item-second"href=""><li><span>不限</span></li></a>
+    			<a href=""><li>展会论坛</li></a>
+    			<a href=""><li>互动沙龙</li></a>
+    			<a href=""><li>项目路演</li></a>
+    			<a href=""><li>培训讲座</li></a>
+    			<a href=""><li>走访考察</li></a>
+    			<a href=""><li>线上分享</li></a>
+    			<a href=""><li>其他</li></a>
+    		</ul>
+    	</div><br>
+    	<div class="nav-1">
+    		<div class="nav-1-1">
+    			<img style="width:310px;height:180px" src="https://res4.maikeji.cn/FssbvCnaFfUFW8ScbkpJ4cdxqXHl" alt="">
+    		</div>
+    		<div class="nav-1-2">
+    			<ul>
+    				<li id="filter">与资本零距离|创合会"商学院好项目"第十期路演会</li>
+    				<li>活动时间：2018.12.09-2018.12.09</li>
+    				<li>简介：迈科技 创合会强强联合，携手百家投资机构，助力创业者精准对接，实现创业梦想</li>
+    				<a href=""><button>我要报名</button></a>
+    			</ul>
+    		</div>
+    		 <div class="clear"></div>
+    	</div><br>
+    	<div class="nav-2">
+    		<div class="nav-2-1">
+    			<img style="width:310px;height:180px" src="https://res4.maikeji.cn/FvB-7IMYN14zo1DbhfrOwVwx5IgD" alt="">
+    		</div>
+    		<div class="nav-2-2">
+    			<ul>
+    				<a id="one" href=""><li>【讲座】吴晓波带你解读《避免失败》</li></a>
+    				<a href=""><li>活动时间：2018.12.11-2018.12.13</li></a>
+    				<a href=""><li>简介：先报名享受会员优惠，赠送超值服务</li></a>
+    				<a href=""><button>我要报名</button></a>
+    			</ul>
+    		</div>
+    	</div><br>
+    	<div class="nav-3">
+    		<div class="nav-3-1">
+    			<img style="width:310px;height:180px"  src="https://res4.maikeji.cn/FhQtJGLFWeMtBaA9uiCZYasWDsYD" alt="">
+    		</div>
+    		<div class="nav-3-2">
+    			<ul>
+    				<a id="two" href=""><li>第二届平湖市科技投融资接对接会</li></a>
+    				<a href=""><li>活动时间：201812.11-2018.12.31</li></a>
+    				<a href=""><li>简介：科技赋能，激发创新活力，诚邀各位参加第二届平湖市科技投融资接对接会</li></a>
+    				<a href=""><button>我要报名</button></a>
+    			</ul>
+    		</div>
+    	</div><br>
+    	<div class="nav-4">
+    		<div class="nav-4-1">
+    			<img style="width:310px;height:180px"  src="https://res4.maikeji.cn/Fhk0j9OoOT8IaQsP2pzjodCyS_0Q" alt="">
+    		</div>
+    		<div class="nav-4-2">
+    			<ul>
+    				<a id="three" href=""><li>让技术与市场来一场真正的对话|迈科技公益大讲堂</li></a>
+    				<a href=""><li>活动时间：201812.11-2018.12.31</li></a>
+    				<a href=""><li>简介：不让优质项目买没，真正让技术发挥价值</li></a>
+    				<a href=""><button>我要报名</button></a>
+    			</ul>
+    		</div>
+    	</div><br>
+    	<div class="nav-5">
+    		<div class="nav-5-1">
+    			<img style="width:310px;height:180px"  src="https://res4.maikeji.cn/Fk1kiY55VQDnvqHwxjfnOj8hm2Wm" alt="">
+    		</div>
+    		<div class="nav-5-2">
+    			<ul>
+    				<a id="four" href=""><li>2019届第十一届上海国际化工技术装备展览会</li></a>
+    				<a href=""><li>活动时间：活动时间：201812.11-2018.12.31</li></a>
+    				<a href=""><li>简介：2019届第十一届上海国际化工技术装备展览会开始报名啦</li></a>
+    				<a href=""><button>我要报名</button></a>
+    			</ul>
+    		</div>
+    	</div>
+    	<div class="page">
+    		<span style="background-color:#1f86ed;color:white;">1</span>
+    		<span>2</span>
+    		<span>3</span>
+    		<span>4</span>
+    		<span>5</span>
+    		<span>6</span>
+    		<span>...</span>
+    		<span>27</span>
+    		<span>下一页</span>
+    	</div>
+    </div>
+    <div class="pop">
+    	<div class="top-1">
+    		活动回顾
+    	</div><br>
+    	<div class="top-2">
+    		<img style="width:260px;height:180px;" src="https://res4.maikeji.cn/FpJ8zik8TTGnqipwgNp8m9UdKEtG" alt="">
+    	</div><br>
+    	<div class="top-3">
+    		需求征集中，第三方招聘中...
+    	</div><br>
+    	<div class="top-4">
+    		<img style="width:260px;height:180px;" src="https://res4.maikeji.cn/FkMmpPsTZit5R3ThgUm8LTOL2qpU" alt="">
+    	</div><br>
+    	<div class="top-5">
+    		【公开课】|首届长三角洲科技交
+    	</div><br>
+    	<div class="top-6">
+    		<img style="width:260px;height:180px;"  src="https://res4.maikeji.cn/FoPw9hs-MHTkKhurchvgU0XmaXec" alt="">
+    	</div><br>
+    	<div class="top-7">
+    		有机工业废水如何处理  看看
+    	</div><br>
+    	<div class="top-8">
+    		<img style="width:260px;height:180px;" src="https://www.maikeji.cn/images/wx-qrcode.jpg" alt="">
+    	</div>
+    </div>
+  </div>
 </template>
 <script>
 	
 </script>
 <style>
-	ul  li {
-		list-style:none;
-	}
-	.Activity{
-		margin:0 auto;
-	}
-	.Activity .content{
-		width:80%;
-		height:100%;
-		position:relative;
+.Activity{
+	position:relative;
+}
+.container{
+	width:1000px;
+}
+	.Activity .title{
 		text-align:center;
-		
-
-	}
-	.Activity .title>h1{
+		font-size:25px;
 		color:gold;
-		font-size:40px;
 	}
-	.Activity .text>h2{
-		color:gold;
-		font-size:20px;
-	}
-	.Activity .globnav{
-
-		width:78%;
-		height:50px;
-		background-color:#fafafa;
-		margin:0 auto;
-		line-height: 50px;
-		
-	}
-
-	ul>a>li{
-		display: inline-block;
-		width:80px;
+	
+	.Activity .text{
+		text-align:center;
 		font-size:15px;
-		color:#7ea9ba;
+		color:gold;
 	}
-	ul>a>li:hover{
-		color:#559dcb;
+	.list{
+		background-color:#ededed;
+		height:50px;
+		line-height: 50px;
 	}
-	.Activity #first-item{
-		font-weight: bolder;
-		color:black;
+	.list>ul>a>li{
+		display:inline-block;
+		width:105px;
+
 	}
-	.Activity .list{
+	#item-first{
+		
+		font-weight:bolder;
+	}
+	#item-second span{
+		background-color:lightblue;
+		padding:5px 20px 5px 20px;
+		border:1px solid #4dafed;
+	}
+	.nav-1{
 		position:relative;
-		height:180px;
-		width:840px;
-		
-		margin:0 auto;
-		
-
 	}
-	.Activity .left{
-		width:310px;
-		height:180px;
-		background-color:pink;
-	}
-	.Activity .right{
-		left:350px;
-		text-align:left;
-		width:530px;
-		height:180px;
-		text-align:left;
+	
+	
+	.nav-1-2{
 		position:absolute;
-		top:0;
-		right:0;
-		line-height:35px;
-
+		left:330px;
+		top:0px;
+		line-height:45px;
 	}
-	.Activity #second{
+	#filter{
 		color:black;
 		font-size:18px;
 	}
-	 li>button{
-		background-color: #1f86ed;
-		width:130px;
-		height:35px;
-		border-radius: 10px;
+	button{
+		width:100px;
+		height:40px;
+		border-radius:10px;
+		background-color:#1f86ed;
 		color:white;
-		font-size:18px;
-		cursor: pointer;
+		font-size:15px;
 	}
-	.Activity .item{
-		position:relative;
-		height:180px;
-		width:840px;
-		
-		margin:0 auto;
-	}
-	.Activity .item-1{
-		width:310px;
-		height:180px;
-		background-color:blue;
-	}
-	.Activity .item-2{
-        width:530px;
-		height:180px;
-		left:350px;
-		text-align:left;
-		position:absolute;
-		top:0;
-		right:0;
-		line-height:45px;
-	}
-	.Activity #third{
-		color:black;
-		font-size:18px;
-	}
-	.Activity .meet{
-		position:relative;
-		height:180px;
-		width:840px;
-		
-		margin:0 auto;
-	}
-	.Activity .meet-1{
-		width:310px;
-		height:180px;
-		background-color:red;
-	}
-	.Activity .meet-2{
-		 width:530px;
-		height:180px;
-		left:350px;
-		text-align:left;
-		position:absolute;
-		top:0;
-		right:0;
-		line-height:45px;
-	}
-	.Activity #forth{
-		color:black;
-		font-size:18px;
-	}
-  .Activity .filter{
-  	    position:relative;
-		height:180px;
-		width:840px;
-		
-		margin:0 auto;
-  }
-  .Activity .filter-1{
-  		width:310px;
-		height:180px;
-		background-color:teal;
-  }
-  .Activity .filter-2{
-  	 	width:530px;
-		height:180px;
-		left:350px;
-		text-align:left;
-		position:absolute;
-		top:0;
-		right:0;
-		line-height:45px;
-  }
-  .Activity #fifth{
-  		color:black;
-		font-size:18px;
-  }
-  .Activity .read{
-  		position:relative;
-		height:180px;
-		width:840px;
-		
-		margin:0 auto;
-  }
-  .Activity .read-1{
-  		width:310px;
-		height:180px;
-		background-color:blue;
-  }
-  .Activity .read-2{
-  		width:530px;
-		height:180px;
-		left:350px;
-		text-align:left;
-		position:absolute;
-		top:0;
-		right:0;
-		line-height:45px;
-  }
-  .Activity #sixth{
-  		color:black;
-		font-size:18px;
-  }
-  .Activity .page{
-  		position:relative;
-		height:30px;
-		width:840px;
-		left:300px;
-		margin:0 auto;
-  }
-  .Activity .page>ul>li{
-  	float:left;
-  	width:60px;
-  	font-size:18px;
-  	color:black;
-
-  }
-  .Activity .page>ul>li:hover{
-  	color:#1f86ed;
-  	cursor:pointer;
-  }
-  .Activity .picture{
-  	width:310px;
-  	height:180px;
-  }
- .Activity .rightnav{
- 	position: absolute;
-	box-sizing:border-box;
-	width:23%;
-	height:1080px;
-	/* border:1px solid #ccc; */ 
-	right:15px;
-	top:20%;
-	margin:0 auto;
+  a button:hover{
+	background-color: rgb(40, 96, 144);
+	color:white;
  }
-  .Activity .top{
+ .nav-2{
+ 	position:relative;
+ }
+.nav-2-2{
+		position:absolute;
+		left:330px;
+		top:0px;
+		line-height:45px;
+}
+#one{
+		color:black;
+		font-size:18px;
+}
+.nav-3{
+	position:relative;
+}
+.nav-3-2{
+		position:absolute;
+		left:330px;
+		top:0px;
+		line-height:45px;
+}
+.nav-4{
+	position:relative;
+}
+.nav-4-2{
+		position:absolute;
+		left:330px;
+		top:0px;
+		line-height:45px;	
+}
+#two{
+		color:black;
+		font-size:18px;
+}
+#three{
+		color:black;
+		font-size:18px;
+}
+.nav-5{
+	position:relative;
+}
+.nav-5-2{
+		position:absolute;
+		left:330px;
+		top:0px;
+		line-height:45px;	
+}
+.page {
+	margin-left:500px;
 
-  	width:100%;
-  	height:50px;
-  	background-color:#fafafa;
-  	text-align:center;
-  	line-height: 50px;
-  	font-size:18px;
-  }
-.Activity .top-1{
-	margin-top:30px;
 }
-.Activity .top-2{
+ span{
+	font-size:18px;
+	color:#1f86ed;
+	padding-left:10px;
+	padding-right:10px;
+	border:2px solid #d6d6d6;
+	border-radius:2px;
+
+	
+
+}
+span:hover{
 	background-color:grey;
+	cursor:pointer;
+}
+.pop{
+	width:260px;
+	height:950px;
+	
+	position:absolute;
+	right:-25px;
+	top:100px;
+
+}
+
+.top-1{
+	margin-top:-3px;
 	text-align:center;
-	margin-top:30px;
-	height:50px;
 	line-height:50px;
-	font-size:16px;
+	font-size:18px;
+	background-color:#ededed;
+}
+.top-3{
+	text-align:center;
+	line-height:50px;
+	font-size:18px;
+	background-color:#ededed;
 	color:white;
 }
-.Activity .top-3{
-	margin-top:30px;
-}
-.Activity .top-4{
-	background-color:grey;
+.top-5{
 	text-align:center;
-	margin-top:30px;
-	height:50px;
 	line-height:50px;
-	font-size:16px;
+	font-size:18px;
+	background-color:#ededed;
 	color:white;
 }
-.Activity .top-5{
-	margin-top:30px;
-}
-.Activity .top-6{
-	background-color:grey;
+.top-7{
 	text-align:center;
-	margin-top:30px;
-	height:50px;
 	line-height:50px;
-	font-size:16px;
+	font-size:18px;
+	background-color:#ededed;
 	color:white;
 }
+
 </style>
