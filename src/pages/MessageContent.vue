f7ab1e564dc4e306cdd9841fb765ec6e235c2229
licy@briup.com <pluslicy>
2018-12-05 13:57:49 +0800

    第二期内容

---
+++ src/pages/MessageContent.vue
@@ -0,0 +1,6 @@
+<!--资讯详细页面-->
+<template>
+  <div class="messageContent">
+    资讯详细页面
+  </div>
+</template>
---
+++ src/pages/TechnologyContent.vue
@@ -0,0 +1,6 @@
+<!-- 技术详细页面 -->
+<template>
+  <div class="technologyContent">
+    技术详细页面
+  </div>
+</template>
