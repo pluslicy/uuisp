cc611ed10f128ac2f812eedab1484656b0c2d052
daiannawoaini <leiyanjun3@163.com>
2018-12-06 09:22:47 +0800

    init

--- src/pages/ThinkTank.vue
+++ src/pages/ThinkTank.vue
@@ -1,177 +1,294 @@
 <!-- 首页 -->
 <template>
-	<div class="thinkTank">
-		<div class="container">
-		<div class="cve">
-			有问题 找专家
-		</div><br>
-		<div class="link">
-			Link Technology to Industry
-		</div><br>
-<div class="mixed">
-		<div class="inp" style="text-align:center;vertical-align:middel;">
-			<input  style="width:600px;height:40px;font-size:18px; color:#ccc;"type="" value="寻找你的专家顾问">
-			<div class="btn">
-				<button style="width:100px;height:48px;position:absolute;left:845px;top:-48px;background-color:#156fb8;"><span style="color:white;font-size:20px">搜&nbsp;索</span></button>
-			</div>
-			</div>
-		</div>
-		<div class="home">
-			<span>入驻专家</span>
-		</div>
-		<div class="load">
-			<span style="color:#ccc">知名学者、产业专家，为您技术升级转型之路出力</span>
-		</div><br>
+	<div class="ThinkTank">
+		<div class="content" style="border:1px solid #ccc">
+			<div class="biaoti">
+				<h1>有问题&nbsp;&nbsp;找专家</h1>
+			</div><br>
+			<div class="fubiao">
+				<h2>Link   Technology   to   Industry</h2>
+			</div><br>
+			<div class="input" style="text-align:center;vertical-align:middel;">
+				<input  style="width:610px;height:40px;font-size:18px; color:#ccc;" type="text" value="寻找你的专家顾问">
+				<i>搜&nbsp;&nbsp;&nbsp;&nbsp;索</i>
+			</div><br>
+			<br>
+			<br>
+			<div class="zhuanjia">
+				<i>入&nbsp;&nbsp;驻&nbsp;&nbsp;专&nbsp;&nbsp;家</i>
+			</div><br>
+			<br>
+			<br>
+			<div class="title-1">
+				知名学者、产业专家，为您技术升级转型之路出力
+			</div><br>
+			<br>
+			<br>
+			<div class="title-2">
+				<i>医药科学&nbsp;&nbsp;&nbsp;</i><i>化学化工&nbsp;&nbsp;&nbsp;</i><i>工程技术&nbsp;&nbsp;&nbsp;</i><i>农业科学</i>
+			</div><br>
+			<br>
+			<br>
+			<br>
+			<div class="pic">
+				<div class="pic-1">
+					<div class="model">
+						<ul>
+							<li><i style="border:1px solid white; padding:10px">新型药物</i>&nbsp;&nbsp;&nbsp;&nbsp;<i style="border:1px solid white;padding:10px">呼吸性疾病</i></li>
+						</ul>
+					</div>
+					<img style="width:300px;height:330px" src="https://res4.maikeji.cn/FlcdTlvZyfwZe8gLMBqMPwTagWoa" alt="">
+					<div style="width:300px;height:80px;background-color: white" class="name">
+						<ul>
+							<li><b>钱峰</b> 研究员</li>
+							<li>上海交通大学|药学院</li>
+						</ul>
+					</div>
+				</div>
+				<div class="pic-2">
+					<div class="model-1">
+						<ul>
+							<li><i style="border:1px solid white; padding:10px">药物化学</i>&nbsp;&nbsp;&nbsp;&nbsp;<i style="border:1px solid white;padding:10px">慢性疼痛药</i></li>
+						</ul>
+					</div>
+					<img style="width:300px;height:330px" src="https://res4.maikeji.cn/Fjfipb1pUJxqzNm12bdiE-ZDx4HO"  alt="">
+					<div style="width:300px;height:80px;background-color: white" class="name-1">
+						<ul>
+							<li><b>傅磊</b> 教授，博导</li>
+							<li>上海交通大学|药学院</li>
+						</ul>
+					</div>
+				</div>
+				<div class="pic-3">
+					<div class="model-3">
+						<ul>
+							<li><li><i style="border:1px solid white; padding:10px">尼古丁</i>&nbsp;&nbsp;&nbsp;&nbsp;<i style="border:1px solid white;padding:10px">食品</i></li></li>
+						</ul>
+					</div>
+					<img style="width:300px;height:330px" src="https://res4.maikeji.cn/Fs88q36P3SlCS025FiNIOK9_sAqg" alt="">
+					<div style="width:300px;height:80px;background-color: white" class="name-3">
+						<ul>
+							<li><b>许平</b> 教授</li>
+							<li>上海交通大学|生命与科学技术学院</li>
+						</ul>
+					</div>
+				</div>
+				<div class="pic-4">
+					<div class="model-4">
+						<ul>
+							<li><li><li><i style="border:1px solid white; padding:10px">药物输送</i>&nbsp;&nbsp;&nbsp;&nbsp;<i style="border:1px solid white;padding:10px">新型药品</i></li></li></li>
+						</ul>
+					</div>
+					<img style="width:300px;height:330px" src="https://res4.maikeji.cn/Fn6QnLSS8ORdQdT3C4eMC2ngB6NW"  alt="">
+					<div style="width:300px;height:80px;background-color: white" class="name-4">
+						<ul>
+							<li><b>郭圣荣</b> 教授</li>
+							<li>上海交通大学|药学院</li>
+						</ul>
+					</div>
+				</div>
+			</div><br><br><br>
+			<a href=""><div class="morethan">
+				查看更多医药科学类专家
+			</div></a><br>
+			<br>
+			<br>
+			<div class="help">
+				合作机构
+			</div><br>
 		
-		<div class="hote">
-			<ul class="sub-nav">
-				<a href=""><li>医药科学.</li></a>
-				<a href=""><li>化学化工.</li></a>
-				<a href=""><li>工程技术.</li></a>
-				<a href=""><li>农业科学.</li></a>
-			</ul>
-
-		</div><br>
-		<div class="expert">
-			
-			<ul id="medial">
-				<li><img class="expert-1" src="https://res4.maikeji.cn/FlcdTlvZyfwZe8gLMBqMPwTagWoa" alt="">
-					<div class="xxx"></div>
-				</li>
-				<li><img class="expert-1" src="https://res4.maikeji.cn/Fjfipb1pUJxqzNm12bdiE-ZDx4HO" alt="">
-					<div class="xxx"></div>
-				</li>
-				<li><img class="expert-1" src="https://res4.maikeji.cn/Fs88q36P3SlCS025FiNIOK9_sAqg" alt="">
-					<div class="xxx"></div>
-				</li>
-				<li><img class="expert-1" src="https://res4.maikeji.cn/Fn6QnLSS8ORdQdT3C4eMC2ngB6NW" alt="">
-					<div class="xxx"></div>
-				</li>
-			</ul>
-			
-			
-
-
+			<br>
+			<div class="toget">
+				汇聚国内外顶尖高校和科研院所的优质技术力量
+			</div><br><br>
+			<div class="boot">
+				
+			</div>
+			<br><br>
+			<div class="look">
+				查看更多合作机构
+			</div>
 		</div>
-</div>
+
 	</div>
 </template>
 <script>
-	export default {
-		data (){
-			return{
-
-			}
-		}
-	}
+	
 </script>
 <style>
-ul li {
-	list-style:none;
-}
-.container{
-	width:80%;
-	height:100%;
-	
-	margin:0 auto;
-}
-.mixed{
-	width:100%;
-	height:100%;
-	margin:0 auto;
-	background-color:red;
+.content{
 	position:relative;
-	
-	
-
+	background-color:#ebe9ea;
 }
-.btn{
-	position:absolute;
-	right:1100px;
-	margin:0 auto;
-
-}
-
-
-	.cve{
-		text-align: center;
-		font-size:30px;
-		color:gold;
-	}
-	.link{
+	.content .biaoti{
 		text-align:center;
-		color:lightblue;
-		font-size:20px;
+		font-size:16px;
+		color:white;
 	}
-	.home{
-		text-align: center;
+	.content .fubiao{
+		text-align:center;
+		font-size:12px;
+		color:#a9dbfe;
+	}
+	.input i{
+		margin-left:-5px;
+		background-color:#156fb8;
+		padding:12px 13px 12px 20px;
+		color:white;
+		font-size:18px;
+	}
+	.input i:hover{
+		cursor:pointer;
+		background-color:#395a77;
+	}
+	.zhuanjia {
+		text-align:center;
+		
+	}
+	.zhuanjia i{
+		font-weight: lighter;
+		font-size:40px;
+		color:#333333;
+	}
+	.title-1{
+		text-align:center;
+		color:#333333;
+	}
+	.title-2{
+		text-align:center;
 		font-size:30px;
-		margin-top:30px;
+	}
+	.title-2 i:hover{
+		color:#156fb8;
+		cursor:pointer;
+	}
+
+	.pic .pic-2{
+		position:absolute;
+		left:310px;
+		top:510px;
+
+	}
+	.pic .pic-3{
+		position:absolute;
+		left:620px;
+		top:510px;
+	}
+	.pic .pic-4{
+		position:absolute;
+		left:930px;
+		top:510px;
+	}
+	.name{
+		text-align:center;
+		line-height:40px;
+		font-size:16px;
+	}
+	.name-1{
+		text-align:center;
+		line-height:40px;
+		font-size:16px;
+	}
+	.name-2{
+		text-align:center;
+		line-height:40px;
+		font-size:16px;
+	}
+	.name-3{
+		text-align:center;
+		line-height:40px;
+		font-size:16px;
+	}
+	.name-4{
+		text-align:center;
+		line-height:40px;
+		font-size:16px;
+	}
+	.pic-1{
+		position:relative;
+	}
+	.model{
+
+		color:white;
+		line-height:410px;
+		text-align:center;
+		position:absolute;
+		width:300px;
+		height:410px;
+		background:rgba(0,0,0,0.5);
+		 display:none; 
+	}
+	.pic-1:hover .model{
+		display:block;
+		cursor:pointer;
+	}
+	.model-1{
+		color:white;
+		line-height:410px;
+		text-align:center;
+		position:absolute;
+		width:300px;
+		height:410px;
+		background:rgba(0,0,0,0.5);
+		display:none;
+	}
+	.pic-2:hover .model-1{
+		display:block;
+		cursor:pointer;
+	}
+	.model-3{
+		color:white;
+		line-height:410px;
+		text-align:center;
+		position:absolute;
+		width:300px;
+		height:410px;
+		background:rgba(0,0,0,0.5);
+		display:none;
+	}
+	.pic-3:hover .model-3{
+		display:block;
+		cursor:pointer;
+	}
+	.model-4{
+		color:white;
+		line-height:410px;
+		text-align:center;
+		position:absolute;
+		width:300px;
+		height:410px;
+		background:rgba(0,0,0,0.5);
+		display:none;
+	}
+	.pic-4:hover .model-4{
+		display:block;
+		cursor:pointer;
+	}
+	.morethan{
+		text-align:center;
+		font-size:25px;
+		color:#156fb8;
+	}
+	.help{
+		text-align:center;
+		font-size:40px;
+		color:#333333;
+	}
+	.toget{
+		text-align:center;
 		color:black;
 	}
-	.load{
-		text-align: center;
-		font-size:14px;
-		margin-top:20px;
-		color:black;
+	.boot{
+		width:1240px;
+		height:600px;
+		background-color: lightblue;
 	}
-	.hote{
-		width:100%;
-		height:100px;
-		
-		margin:0 auto;
+	.look{
 		text-align:center;
+		font-size:25px;
+		color:#156fb8;
 	}
-	.sub-nav>a>li{
-		display:inline-block;
-		width:160px;
-		line-height: 100px;
-		font-size:30px;
-		font-weight:bolder;
-		
-		
-		}
-		.sub-nav>a>li:hover{
-			color:#59a8d7;
-		}
-		.expert{
-			width:100%;
-			height:400px;
-			position:relative;
-			margin:0 auto;
-			display:flex;
-			
-		}
-		.expert>ul>li{
-			display:inline-block;
-			flex:1;
-			position:relative;
-		}
-		.expert-1{
-			width:260px;
-			height:400px;
-		}
-
-		#medial{
-			margin:0 auto;
-		}
-		.expert-1{
-			
-			width:260px;
-			height:400px;
-		}
-		.xxx{
-			width:260px;
-			height:400px;
-			 background: rgba(101, 101, 101, 0.4);
-
-			position:absolute;
-			left:0px;
-				top:0;
-			display:none; 
-		}
-		.expert:hover .xxx{
-			display:block;
-		}
-
+	.look:hover{
+		cursor:pointer;
+	}
 </style>
--- src/pages/ThinkTankContent.vue
+++ src/pages/ThinkTankContent.vue
@@ -1,6 +1,169 @@
 <!--专家详细页面-->
 <template>
   <div class="thinkTankContent">
-    专家详细页面
+    <div style="border:1px solid #ccc" class="top-pic">
+    	<div class="top-subnav">
+    	<ul>
+    		<a href=""><li>首页></li></a>
+    		<a href=""><li>智库></li></a>
+    		<a href=""><li>专家详情></li></a>
+    	</ul>
+    	</div><br>
+    	<div class="top-pic-1">
+    		<img src="" alt="">
+    		<div class="top-pic-2">
+    			<img src="" alt="">
+    		</div>
+    	</div><br>
+    	<br>
+    	<br>
+    	<br>
+    	<div class="talent">
+    		<ul>
+    			<li style="color:#333333;font-size:20px;"><b>钱峰</b></li>
+    			<li style="font-size:16px;">研究员&nbsp;&nbsp;|&nbsp;&nbsp;上海交通大学</li>
+    		</ul>
+    	</div><br>
+    	<div class="wenzhang">
+    		<ul>
+    			<li style="font-size:18px">概况</li>
+    			<li style="font-size:14px">实验室简要介绍</li>
+    			<li style="font-size:18px">代表成果</li>
+    			<li style="font-size:14px">突出研究成果</li>
+    			<li style="font-size:18px">教学工作</li>
+    			<li style="font-size:14px">本科生：《微生物学与免疫学》</li>
+    			<li style="font-size:14px">研究生：《免疫药理学》</li>
+    		</ul>
+    	</div><br>
+    	<div class="place">
+    		<ul>
+    			<li style="font-size:18px">研究领域</li>
+    			<li>（1）巨噬细胞和嗜中性粒细胞活化的分子机制与新型药物靶标。</li>
+    			<li>（2）细菌感染、哮喘、肺纤维化等呼吸性疾病发病机理与分子机制。</li>
+    			<li>（3）恶性肿瘤发生发展的病理分子机制与药物靶标。</li>
+    			<li>（4）新型药物筛选体系建立与高通量药物筛选。</li>
+    		</ul>
+    	</div><br>
+    	<div class="thing">
+    		<ul>
+    			<li style="font-size:18px">大事记</li>
+    			<li>（1）巨噬细胞和嗜中性粒细胞活化的分子机制与新型药物靶标。</li>
+    			<li>（2）细菌感染、哮喘、肺纤维化等呼吸性疾病发病机理与分子机制。</li>
+    			<li>（3）恶性肿瘤发生发展的病理分子机制与药物靶标。</li>
+    			<li>（4）新型药物筛选体系建立与高通量药物筛选。</li>
+    		</ul>
+    	</div><br>
+    	<div class="lunwen">
+    		<ul>
+    			<li style="font-size:18px">论文</li>
+    			<li>1 Role for platelet glycoprotein Ib-IX and effects of its inhibition in endotoxemia-induced thrombosis, thrombocytopenia, and mortality.Hong Yin, Alesander Zakharov, Weidong Xu, Adam Corken, Alexander Zakharov, Feng Qian, Sasha Pavlovic, Alekasandar Krbanjevic, Alexander Lyubimov, Zaijie J. Wang, Xiaoping Du.Arterioscler Thromb Vasc Biol.</li><br>
+    			<li>2 A critical role for the guanine nucleotide exchange factor P-Rex1 in platelet secretion and aggregation.Feng Qian, Guy C. Le Breton, Jia Chen, Jing Deng, John W. Christman, Dianqing Wu, Richard D. Ye.Arterioscler Thromb Vasc Biol.</li><br>
+    			<li>3 MAP Kinase Phosphatase 5 protests against sepsis-induced acute lung injury.Feng Qian*, Jing Deng, Benjamin N. Gantner, Chen Dong, John W. Christman, Richard D Ye..American Journal of Physiology-Lung Cellular and Molecular Physiology.</li><br>
+    			<li>4 The Akt1 isoform is required for optimal IFN-b transcription through direct phosphorylation of b-catenin.Benjamin N Gantner, Huali Jin, Feng Qian, Nissim Hay, Bin He, Richard D Ye.J Immunol.</li><br>
+    			<li>5 Protective role of reactive oxygen species in endotoxin-induced lung inflammation through modulation of IL-10 expression.Jing Deng, Xuerong Wang, Feng Qian, Stephen M. Vogel, Lei Xiao, Ravi Ranjan, Hyesuk Park, Manjula Karpurapu, Richard D. Ye, Gye Young Park, John W. Christman.J Immunol.</li><br>
+    			<li>6 A critical role for P-Rex1 in endothelial junction disruption and vascular hyper-permeability.Ram Naikawadi, Ni Cheng, Stephen M. Vogel, Feng Qian, Dianqing Wu, Asrar B. Malik, Richard D Ye.Circulation Research.</li><br>
+    			<li>7 TRPM2 regulation of membrane potential modulates NADPH oxidase-derived ROS production in phagocytes.Anke Di, Xiao-Pei Gao, Feng Qian, Takeshi Kawamura, Jin Han, Claudie Hecquet, Richard D. Ye, Stephen M. Vogel, Asrar B. Malik.Nature Immunology.</li><br>
+    			<li>8 Notch-1 signaling facilitates surviving expression in human non-small cell lung cancer cells.Yuqing Chen, Dianming Li, Hongli Liu, Huanbai Xu, Honghua Zheng, Feng Qian, Wei Li, Chengling Zhao, Zhenhua Wang, Xiaojing Wang.Cancer Biol Ther.</li><br>
+    			<li>9 Polymerization of MIP-1 chemokine (CCL3 and CCL4) and clearance of MIP-1 by insulin-degrading enzyme.Min Ren, Qing Guo, Liang Guo, Martin Lenz, Feng Qian, Rory R. Koenen, Hua Xu, Alexander B. Schilling, Christian Weber, Richard D. Ye, Aaron R. Dinner, Wei-Jen Tang.EMBO J.</li><br>
+    			<li>10 A non-redundant role for MKP5 inlimiting ROS production and preventing LPS-induced vascular injury.Feng Qian, Jing Deng, Ni Cheng, Emily J. Welch, Yongliang Zhang, Asrar B. Malik, Richard A. Flavell, Chen Dong, Richard D. Ye.EMBO J.</li><br>
+    			<li>11 Phosphatase of regenerating liver-3 localizes to cyto-membrane and is required for B16F1 melanoma cell metastasis in vitro and in vivo.Ran Song, Feng Qian, Yu-Pei Li, Xia Sheng, Shao-Xian Cao, Qiang Xu.PLoS ONE.</li><br>
+    			<li>12 PRL-3 siRNA inhibits the metastasis of B16-BL6 mouse melanoma cells in vitro and in vivo.Feng Qian, Yu-Pei Li, Xia Sheng, Zi-Chao Zhang, Ran Song, Wei Dong, Shao-Xian Cao, Zi-Chun Hua, Qiang Xu.Mol. Med.</li><br>
+    			<li>13 Identification of a new metabolite of Astilbin,3’-O-Methylastilbin, and its immunosuppressive activity against contact dermatitis.Jianming Guo, Feng Qian, Jianxin Li, Qiang Xu, Ting Chen.Chinical Chemistry.</li><br>
+    			<li>14 Nitric oxide inhibits T cell adhesion and migration through down-regulating β1 integrin expression in immunological liver injured mice.Yang Sun, Jianli Liu, Feng Qian, Qiang Xu.Intl. Immunopharmacol.</li><br>
+    			<li>15 Interaction between integrin α5 and fibronectin is required for metastasis of B16F10 melanoma cells.Feng Qian, Zi-Chao Zhang, Xue-Feng Wu, Yu-Pei Li, Qiang Xu.Biochem. Biophys. Res. Commun.</li>
+    		</ul>
+    	</div><br><br>
+		<div class="an">
+			<ul>
+				<li style="">新型药物</li>
+				<li>呼吸性疾病发病机理与分子机制</li>
+				<li>高同量药物</li>
+			</ul>
+		</div>
+    </div>
+    <div class="pageone">
+    	<div style="padding-left: 15px;font-size:16px;" class="server">
+    		<b>专家推荐服务</b>
+    	</div>
+    </div>
   </div>
 </template>
+<script>
+	
+</script>
+<style>
+.thinkTankContent{
+	background-color:#eeeeee;
+	position: relative;
+}
+.top-pic{
+	width:1000px;
+
+}
+	.top-subnav>ul>a>li{
+		display: inline-block;
+		
+	}
+	.top-pic-1{
+		width:1000px;
+		height:200px;
+		background-color:orange;
+		position:relative;
+	}
+	.top-pic-2{
+		top:120px;
+		left:420px;
+		position: absolute;
+		width:150px;
+		height:150px;
+		background-color: teal;
+		border-radius: 50%
+	}
+	.talent{
+		text-align:center;
+	}
+	.wenzhang{
+		margin-left: 150px;
+		line-height: 30px;
+	}
+	.place{
+		margin-left: 150px;
+		line-height: 30px;
+	}
+	.thing{
+		margin-left: 150px;
+		line-height: 30px;
+	}
+	.lunwen{
+		margin-left: 150px;
+		line-height: 20px;
+
+	}
+	.lunwen>ul>li{
+		font-size:16px;
+	}
+	.an{
+		text-align: center;
+	}
+	.an>ul>li{
+		display:inline-block;
+		width:200px;
+		border:1px solid #1f86ed;
+		border-radius: 10px;
+		padding-top:5px;
+		padding-bottom:5px;
+
+	}
+	.an>ul>li:hover{
+		cursor:pointer; 
+	}
+	.pageone{
+		position:absolute;
+		border:1px solid red;
+		width:240px;
+		height:850px;
+		right:0;
+		top:40px;
+		background-color: white
+	}
+</style>
