7632028654c2daf361b7c6c664f76bdd08e2a67b
wangym <wangym>
2018-12-06 17:28:05 +0800

    ‘qiyefuwumokuai'

--- src/pages/Enterprise.vue
+++ src/pages/Enterprise.vue
@@ -1,6 +1,619 @@
 <!--企业服务页面-->
 <template>
   <div class="enterprise">
-      企业服务页面
+      <section class="service-section">
+      	<!--第一部分 ：一大核心-->
+		<div class="oneCore">
+			<div class="oneText">
+				<div class="coservice">Core Service</div>
+				<div class="chCore">一大核心</div>
+				<div class="span"></div>
+				<div class="title">提升技术实力</div>
+			</div>
+			<ul class="service-card">
+				<li class="fadeInRightBig" style="animation-delay: 0s;">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-pencil-square-o"></i></div>
+							<span>战略规划</span>
+						</div>
+						<div class="service-card-center">
+							<p>从行业趋势、竞对情况、技术项目、项目申报补贴等方面分析企业现状，为企业梳理技术创新的思路，制定技术创新实施方案。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig" style="animation-delay: 0.1s;">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-list"></i></div>
+							<span>项目推荐</span>
+						</div>
+						<div class="service-card-center">
+							<p>拓宽企业对优质项目的获取渠道，及时把握优质项目商机。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig" style="animation-delay: 0.2s;">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-file-archive-o"></i></div>
+							<span>调研评估</span>
+						</div>
+						<div class="service-card-center">
+							<p>为企业在引进新技术、投资新项目提供前期技术、团队等全方位关联信息调研评估，并在新项目实施过程中提供技术监理等服务。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig" style="animation-delay: 0.3s;">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-life-buoy "></i></div>
+							<span>资源集成</span>
+						</div>
+						<div class="service-card-center">
+							<p>为企业引入技术开发密切相关的技术资源要素，例如技术专家，实验场地等等，为企业低成本高效快速的技术开发提供资源匹配服务。 </p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig" style="animation-delay: 0.4s;">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-flask"></i></div>
+							<span>工艺升级</span>
+						</div>
+						<div class="service-card-center">
+							<p>为企业制订产品升级方案，帮助企业解决产品升级中遇到的技术难题，帮助企业将引进的技术进行消化、吸收、二次创新 。 </p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig" style="animation-delay: 0.5s;">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-cog"></i></div>
+							<span>新品开发</span>
+						</div>
+						<div class="service-card-center">
+							<p>为企业委托开发的项目（产品）制订研发实施计划，帮助企业用高新技术和先进适用技术改造传统产业。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+
+			</ul>
+		</div>
+		<!--第二部分：两大配套-->
+		<div class="twoSet">
+			<div class="twoSetTitle">
+				<div class="coservice">Supporting service</div>
+				<div class="chCore">两大配套</div>
+				<div class="span"></div>
+				<div class="title">梳理品牌形象</div>
+			</div>
+			<ul class="service-card">
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-handshake-o"></i></div>
+							<span>产学研合作</span>
+						</div>
+						<div class="service-card-center">
+							<p>提供与高校一级行政单位产学研合作渠道；建立企业技术背书。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-sign-out"></i></div>
+							<span>专利转让</span>
+						</div>
+						<div class="service-card-center">
+							<p>专利大数据搜索引擎助力，帮助企业最快找到合适的专利，提供全套转让服务。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-lightbulb-o"></i></div>
+							<span>专利申请</span>
+						</div>
+						<div class="service-card-center">
+							<p>提供全套专利申请专业服务，帮助企业进行资料整理，递交，审查辅导，全流程为企业保驾护航。</p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-opera"></i></div>
+							<span>软著申请</span>
+						</div>
+						<div class="service-card-center">
+							<p>提供全套软著申请专业服务，帮助企业进行资料整理，递交，审查辅导，全流程为企业保驾护航。 </p>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+			</ul>
+			<div class="title-wraper">
+				<div class="service-section-sub-title">政策咨询服务</div>
+			</div>
+			<ul class="service-card">
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-check-square-o"></i></div>
+							<span>高企认定</span>
+						</div>
+						<div class="service-card-center">
+							<div class="itemContent">
+								<div class="item"><span>•</span> 政策解读</div>
+								<div class="item"><span>•</span> 准备指导</div>
+								<div class="item"><span>•</span> 材料编制</div>
+								<div class="item"><span>•</span> 辅助申报</div>
+							</div>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-thumbs-o-up"></i></div>
+							<span>技术中心认定</span>
+						</div>
+						<div class="service-card-center">
+							<div class="itemContent">
+								<div class="item"><span>•</span> 政策解读</div>
+								<div class="item"><span>•</span> 准备指导</div>
+								<div class="item"><span>•</span> 材料编制</div>
+								<div class="item"><span>•</span> 辅助申报</div>
+							</div>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-cny"></i></div>
+							<span>创新基金辅导</span>
+						</div>
+						<div class="service-card-center">
+							<div class="itemContent">
+								<div class="item"><span>•</span> 政策解读</div>
+								<div class="item"><span>•</span> 准备指导</div>
+								<div class="item"><span>•</span> 材料编制</div>
+								<div class="item"><span>•</span> 辅助申报</div>
+							</div>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+				<li class="fadeInRightBig">
+					<div class="service-card-container">
+						<div class="service-card-top">
+							<div><i class="fa fa-wrench"></i></div>
+							<span>技改项目申报</span>
+						</div>
+						<div class="service-card-center">
+							<div class="itemContent">
+								<div class="item"><span>•</span> 政策解读</div>
+								<div class="item"><span>•</span> 准备指导</div>
+								<div class="item"><span>•</span> 材料编制</div>
+								<div class="item"><span>•</span> 辅助申报</div>
+							</div>
+						</div>
+						<div class="service-card-footer">立即咨询</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<!--第三部分：年会会员-->
+		<div class="threeMember">
+			<div class="threeTitle">
+				<div class="coservice">Annual fee member</div>
+				<div class="chCore">年费会员</div>
+				<div class="span"></div>
+				<div class="title">四大会员权益</div>
+			</div>
+			<ul class="service-product">
+				<li>
+					<div>
+						<i class="fa fa-stethoscope"></i>
+						<span>体检</span>
+					</div>
+					<div>
+						<span>摸排企业现状</span>
+						<br>
+						<span>协助企业明晰创新路径</span>
+					</div>
+				</li>
+				<li>
+					<div>
+						<i class="fa fa-user-circle"></i>
+						<span>管家</span>
+					</div>
+					<div>
+						<span>全年项目推荐</span>
+						<br>
+						<span>解答相关服务实操问题</span>
+					</div>
+				</li>
+				<li>
+					<div>
+						<i class="fa fa-eercast"></i>
+						<span>赋能</span>
+					</div>
+					<div>
+						<span>颁发荣誉匾牌</span>
+						<br>
+						<span>共享平台技术专家资源</span>
+					</div>
+				</li>
+				<li>
+					<div>
+						<i class="fa fa-balance-scale"></i>
+						<span>优惠</span>
+					</div>
+					<div>
+						<span>享受优惠折扣</span>
+						<br>
+						<span>全方位支持定制化服务</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<!--第四部分：价格咨询-->
+		<div class="fourConsult">
+			<div class="service-label">￥5000/年</div>
+			<div class="service-button">立即咨询</div>
+		</div>
+		<div class="service-section-title-simple">他们来了 你来不来!</div>
+      </section>
+      <!--轮播图-->
+      <div class="service-photo-wall-wrap">	
+		<div class="service-photo-wall">
+			<img src="../assets/enterprise/photo-1.jpg">
+			<img src="../assets/enterprise/photo-2.jpg">
+			<img src="../assets/enterprise/photo-3.jpg">
+			<img src="../assets/enterprise/photo-4.jpg">
+			<img src="../assets/enterprise/photo-5.jpg">
+			<img src="../assets/enterprise/photo-6.jpg">
+			<img src="../assets/enterprise/photo-7.jpg">
+			<img src="../assets/enterprise/photo-8.jpg">
+			<img src="../assets/enterprise/photo-9.jpg">
+			<img src="../assets/enterprise/photo-10.jpg">
+			<img src="../assets/enterprise/photo-1.jpg">
+			<img src="../assets/enterprise/photo-2.jpg">
+			<img src="../assets/enterprise/photo-3.jpg">
+			<img src="../assets/enterprise/photo-4.jpg">
+			<img src="../assets/enterprise/photo-5.jpg">
+			<img src="../assets/enterprise/photo-6.jpg">
+			<img src="../assets/enterprise/photo-7.jpg">
+			<img src="../assets/enterprise/photo-8.jpg">
+			<img src="../assets/enterprise/photo-9.jpg">
+			<img src="../assets/enterprise/photo-10.jpg">
+		</div>
+      </div>
   </div>
 </template>
+<script></script>
+<style scoped>
+	.enterprise{
+		text-align: center;
+	}
+	/*轮播图*/
+	.service-photo-wall-wrap{
+		width: 100%;
+		height: 188px;
+		margin-bottom: 75px;
+		position: relative;
+		overflow: hidden;
+
+	}
+	.service-photo-wall-wrap .service-photo-wall{
+		width: 6200px;
+		display: flex;
+		justify-content: space-around;
+		box-sizing: border-box;
+		animation: loop 25s linear infinite;
+	}
+
+	@keyframes loop{
+		0%{
+			transform: translateX(0);
+		}
+		100%{
+			transform: translateX(-3100px);
+		}
+	}
+	/*第四部分样式*/
+	.enterprise .fourConsult{
+		text-align: center;
+		margin-bottom: 70px;
+	}
+	.fourConsult .service-label{
+		height: 50px;
+		font-size: 30px;
+		margin-bottom: 10px;
+		color: rgb(51,51,51)
+
+	}
+	.fourConsult .service-button{
+		display: inline-block;
+		height: 52px;
+		font-size: 26px;
+		line-height: 48px;
+		padding: 0 40px;
+		color: #35a9ff;
+		border: 2px solid #35a9ff;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+	div.service-section-title-simple{
+		text-align: center;
+		font-size: 40px;
+		height: 70px;
+		line-height: 70px;
+		font-weight: 700;
+		color: #333;
+		margin-bottom: 30px;
+	}
+	/*第三部分样式*/
+	.threeMember .service-product{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		height: 220px;
+		margin-bottom: 75px;
+		list-style: none;
+	}
+	.threeMember .service-product li{
+		width: 290px;
+		box-shadow: 0 0 10px #ccc;
+		border-radius: 5px;
+		overflow: hidden;
+	}
+
+	ul.service-product>li>div:first-of-type{
+		height: 70px;
+		padding-top: 15px;
+		text-align: center;
+		font-size: 20px;
+		line-height: 34px;
+		background: linear-gradient(90deg,#417cfe,#33afff);
+		color: #fff;
+		vertical-align: middle;
+	}
+	ul.service-product>li>div:first-of-type i{
+		vertical-align: middle;
+		font-size: 40px;
+		margin-right: 10px;
+		font-weight: lighter;
+	}
+	ul.service-product>li>div:first-of-type span{
+		font-weight: 700;
+		font-size: 26px;
+
+	}
+	ul.service-product>li>div:nth-of-type(2){
+		padding-top: 30px;
+		text-align: center;
+		color: #8a8c8d;
+		font-size: 18px;
+		line-height: 40px;
+	}
+	/*第一部分样式*/
+	.oneText .coservice,.twoSetTitle .coservice,.threeTitle .coservice{
+		color: rgb(238,238,238);
+		font-size: 60px;
+		font-weight: 700;
+		height: 45px;
+		line-height: 85.7167px;
+		position: relative;
+		vertical-align: bottom;
+		white-space: nowrap;
+		word-spacing: 1px;
+		/* z-index: 1; */
+	}
+
+	.oneText .chCore,.twoSetTitle .chCore,.threeTitle .chCore{
+		color: rgb(51,51,51);
+		font-size: 40px;
+		font-weight: 700;
+		letter-spacing: 10px;
+		line-height: 57.15px;
+		margin-bottom: 6px;
+		position: relative;
+		/* transition-delay: 0s;
+		transition-duration: 1s;
+		transition-property: all;
+		transition-timing-function: ease; */
+		word-spacing: 1px;
+		z-index: 2;
+
+	}
+
+	.oneText  .span,.twoSetTitle .span,.threeTitle .span{
+		color: #333;
+		width: 50px;
+		height: 8px;
+		background: linear-gradient(90deg,#80d8ff,#1287ff);
+		border-radius: 3px;
+		margin:0 auto;
+
+	}
+
+	.oneText .title,.twoSetTitle .title,.threeTitle .title{
+		text-align: center;
+		height: 100px;
+		line-height: 70px;
+		font-size: 22px;
+		transition: 1.5s;
+		letter-spacing: 10px;
+		word-spacing: 1px;
+		color: #333;
+	}
+
+	.oneCore .service-card,.twoSet .service-card{
+		display: flex;
+		flex-wrap: wrap;
+		width: 1100px;
+		margin: 0 auto 75px;
+		position: relative;
+		top: 0;
+		transition: 1s;
+		list-style:none;
+		font-size: 14px;
+		color: #333;
+		justify-content: space-between
+	}
+	.twoSet .service-card{
+		margin:0 auto;
+	}
+	.twoSet .service-card .fadeInRightBig{
+		width: 240px;
+		margin-bottom: 0px;
+	}
+
+	.twoSet .title-wraper div{
+		line-height: 100px;
+		font-size: 22px;
+		letter-spacing: 10px;
+		color: #333;
+		word-spacing: 1px;
+
+	}
+	.service-card-center .itemContent{
+		display: flex;
+		flex-wrap: wrap;
+		line-height: 20px;
+		font-size: 14px;
+		color: #8a8c8d;
+		margin-bottom: 5px;
+		position: relative;
+	}
+	.service-card-center .itemContent .item{
+		min-width: 50%;
+		height: 30px;
+		line-height: 20px;
+		word-spacing: 1px
+	}
+	.itemContent .item span{
+		color: #1283f0;
+		font-weight: 800;
+	}
+
+	.service-card .fadeInRightBig{
+		width: 300px;
+		height: 222px;
+		border-radius: 5px;
+		overflow: hidden;
+		transition: box-shadow .5s;
+		margin-bottom: 50px;
+		line-height: 20px;
+		animation-delay: 0s;
+		transform: none;
+		background-color: #F6F6F6;
+	}
+
+	/*第一部分进入动画*/
+	.fadeInRightBig{
+		animation: fadeInRightBig 1.5s ease;
+	}
+	@keyframes fadeInRightBig{
+		0%{
+			opacity: 0;
+			transform: translate3d(2000px,0,0);
+		}
+		100%{
+			opacity: 1;
+			transform: translateZ(0);
+		}
+	}
+
+	.service-card-container{
+		transition: .5s;
+		background-color: #f6f6f6;
+	}
+
+
+	.service-card-container .service-card-top{
+		margin-bottom: 8px;
+		padding-top: 16px;
+		box-sizing: border-box;
+	}
+
+	.service-card-top div{
+		width: 49px;
+		height: 48px;
+		line-height: 48px;
+		margin:0 auto 8px;
+		box-sizing: border-box;
+		color: rgb(51,51,51)
+	}
+	.service-card-top i{
+		font-size: 46px;
+		font-style: normal;
+		line-height: 48px;
+	}
+	.service-card-top span{
+		display: block;
+		height: 30px;
+		text-align: center;
+		font-size: 14px;
+		font-weight: 700;
+		line-height: 30px;
+		color: #5e6266
+	}
+	.service-card-center{
+		width: 96%;
+		height: 108px;
+		background-color: #fff;
+		margin:0 auto 10px;
+		padding: 10px 25px;
+		line-height: 108px;
+		box-sizing: border-box;
+		position: relative;
+	}
+	.service-card-center p{
+		display: inline-block;
+		font-size: 14px;
+		line-height: 20px;
+		color: #8a8c8d;
+		margin-bottom: 5px;
+
+	}
+
+	.service-card-footer{
+		cursor:pointer;
+		width: 126px;
+		height: 35px;
+		margin:0 auto;
+		line-height: 35px;
+		border-radius: 20px;
+		color: #fff;
+		background-color: #1283f0;
+		text-align: center
+
+	}
+		/*item鼠标移入动画*/
+		li.fadeInRightBig:hover{
+			/* cursor: default; */
+			box-shadow: 0px -10px 0px 0px #7AD4FF,   /*上边阴影*/
+                		-10px 0px 0px 0px #7AD4FF,   /*左边阴影*/
+		                10px 0px 0px 0px #7AD4FF,    /*右边阴影*/
+		                0px 10px 0px 0px #7AD4FF;    /*下边阴影*/
+		}
+	
+</style>
